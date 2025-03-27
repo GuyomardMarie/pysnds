18148bf1b2c586661aa2fb39b9a8216b7ff42c18
Marie Guyomard <93378786+GuyomardMarie@users.noreply.github.com>
2025-03-27 14:43:20 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+VERSION = 0.1
+
+setuptools.setup(
+    name="snds_pop",
+    version=VERSION,
+    author="Marie Guyomard",
+    author_email="marie.guyomard83@gmail.com",
+    description="A library to identify and interpret targeted information in the SNDS.")
