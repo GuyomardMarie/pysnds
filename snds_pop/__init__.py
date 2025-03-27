9628cebc87d11b6a8c387f69d29250ec93816fca
Marie Guyomard <93378786+GuyomardMarie@users.noreply.github.com>
2025-03-27 21:35:16 +0100

    Update __init__.py

--- snds_pop/__init__.py
+++ snds_pop/__init__.py
@@ -1,4 +1,5 @@
 from .snds_query import SNDS_Query
 from .snds_treatment import SNDS_Treatment
+from .snds_bc import SNDS_BC
 
-__all__ = ["SNDS_Query", "SNDS_Treatment"]
+__all__ = ["SNDS_Query", "SNDS_Treatment", "SNDS_BC"]
