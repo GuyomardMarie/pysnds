240cb97645777f0d2374a15f8ab743556a7fba2c
Marie Guyomard <93378786+GuyomardMarie@users.noreply.github.com>
2025-06-11 11:19:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,9 @@ setuptools.setup(
     version=VERSION,
     author="Marie Guyomard",
     author_email="marie.guyomard83@gmail.com",
-    description="A library to identify and interpret targeted information in the SNDS.")
+    description="A library to identify and interpret targeted information in the SNDS.",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={
+        "pysnds": ["BC_medical_codes.json"],
+    })
